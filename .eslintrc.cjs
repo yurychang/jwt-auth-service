22fc1108df3fa6c48c09f2562c573cfe401e8b4c
Yury <yuury.chang@gmail.com>
2023-10-19 11:19:42 +0800

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  parserOptions: {
+    sourceType: 'module',
+  },
+  env: { es6: true },
+  extends: ['@yurychang', '@yurychang/typescript', 'prettier'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
+  plugins: ['simple-import-sort', 'import'],
+  rules: {
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+  },
+};
